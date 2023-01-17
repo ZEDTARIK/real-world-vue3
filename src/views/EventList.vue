8af336d7d85d612930ce392ac5b20bdd76506c70
Zouhair <ettarak.zouhair@gmail.com>
2023-01-17 22:43:28 +0100

    add axios with service

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService";
 
 export default {
   name: "EventList",
@@ -16,42 +17,15 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 54151454,
-          category: "animal welfare",
-          title: "Cat Adoption Day",
-          description: "Find your new feline friends at this event",
-          location: "Meow Town",
-          date: "January 28, 2023",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kat Laydee",
-        },
-        {
-          id: 31251564,
-          category: "food",
-          title: "Communication Grding Day",
-          description: "Find your new feline friends at this event",
-          location: "Meow Town",
-          date: "March 15, 2022",
-          time: "12:00",
-          petsAllowed: false,
-          organizer: "Kat Laydee",
-        },
-        {
-          id: 4564565741,
-          category: "Somthing",
-          title: "VueJs3 Training",
-          description: "Find your new feline friends at this event",
-          location: "MORROCO Town",
-          date: "Descember 12, 2022",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kat Laydee",
-        },
-      ],
+      events: null,
     };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .then((error) => console.error(error));
   },
 };
 </script>
