8a19bef6256f966146f9f3d6e4716655be64768b
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-12-01 20:37:51 +0100

    create EventCard and use it

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
+<!-- eslint-disable vue/no-unused-components -->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h2>Event For Good</h2>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
-    HelloWorld,
+    EventCard,
   },
-};
+  data() {
+    return {
+      events: [
+        {
+          id: 15487,
+          category: 'Community Gardening',
+          title: 'Community SETEL',
+          description: 'Community Join us we tend to th community members',
+          location: 'Morocco',
+          date: 'March 1, 2015',
+          time: '10:00',
+          organizer: 'Zouhair ETTARAK',
+        },
+        {
+          id: 136478,
+          category: 'JAva Developper',
+          title: 'Learn Java && Spring Boot',
+          description: 'Community to larn java and spring security',
+          location: 'Morocco',
+          date: 'April 1, 2022',
+          time: '06:00',
+          organizer: 'Zouhair ETTARAK',
+        },
+        {
+          id: 745154,
+          category: 'Learn VueJs',
+          title: 'Learn Vue Js && JavaScript',
+          description: 'learn vuejs from web site and practise itself',
+          location: 'China',
+          date: 'March 1, 2023',
+          time: '10:00',
+          organizer: 'EVAN VUEJS',
+        },
+      ],
+    }
+  },
+}
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
